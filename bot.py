776e12492f0ccb5991eee4d0bb9aebe0e91ca944
Steven Malis <smmalis37@gmail.com>
2022-12-04 05:58:44 -0500

    Use library's loop handling structures.

--- bot.py
+++ bot.py
@@ -1,15 +1,10 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import asyncio
 from datetime import datetime, timedelta
 import functools
 import random
 import typing
-
-
-# Additional logging, if needed
-# import logging
-# logging.basicConfig(level=logging.INFO)
 
 # Ensure messages get logged correctly
 print = functools.partial(print, flush=True)
@@ -22,58 +17,29 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot("!", intents=intents, activity=discord.Game("https://github.com/smmalis37/ventbot"))
 
-# The task that actually does work
-task = None
-
-
-# Run once the client is fully connected and synced
-# If there is a disconnect and reconnect, this will run again
 @client.event
 async def on_ready():
-    global task
-    print("Connected.")
-    if task == None:
-        print("Scheduling task.")
-        # Schedule the process_messages task to run immediately
-        task = asyncio.ensure_future(process_messages())
+    print("Ready!")
+    process_messages.start()
 
-# Run after a disconnect if able to resume the connection instead of needing to make a fresh connection
-@client.event
-async def on_resumed():
-    print("Resumed.")
-    await on_ready()
-
-# Run whenever the client loses connection to discord for any reason
-@client.event
-async def on_disconnect():
-    global task
-    if task != None:
-        print("Lost connection, cancelling.")
-        # Stop the task from running, since it can't do anything right now
-        task.cancel()
-        task = None
-
+@tasks.loop(hours=1)
 async def process_messages():
-    # Repeat forever
-    while True:
-        print("Beginning processing.")
-        # Get the channel we're monitoring
-        channel = client.get_channel(channel_id)
-        print("Reading from " + channel.name)
-        # Calculate the time 7 days ago
-        timestamp = datetime.now() - timedelta(days=7)
-        print("Messages older than " + str(timestamp))
-        # Get all messages before that time
-        messages = await channel.history(before=timestamp, limit=None).flatten()
-        print("Deleting " + str(len(messages)) + " messages.")
-        # Delete the messages
-        for m in messages:
-            if (not m.pinned):
-                await m.delete()
-        # Wait to run again
-        print("Waiting 1 hour.")
-        await asyncio.sleep(60 * 60)
-
+    print("Waiting for ready.")
+    await client.wait_until_ready()
+    print("Beginning processing.")
+    # Get the channel we're monitoring
+    channel = client.get_channel(channel_id)
+    print("Reading from " + channel.name)
+    # Calculate the time 7 days ago
+    timestamp = datetime.now() - timedelta(days=7)
+    print("Messages older than " + str(timestamp))
+    # Get all messages before that time
+    messages = [m async for m in channel.history(before=timestamp, limit=None) if not m.pinned]
+    print("Deleting " + str(len(messages)) + " messages.")
+    # Delete the messages
+    for m in messages:
+        await m.delete()
+    print("Done.")
 
 @client.command()
 async def roll(ctx, count: typing.Optional[int] = 6, max: typing.Optional[int] = None):
